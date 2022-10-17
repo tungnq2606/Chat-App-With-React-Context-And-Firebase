91a2faf617930afd05340b2c1389e58bfe336bd9
quoctung <tungnq2606@gmail.com>
2022-10-17 09:43:15 +0700

    update style gifted chat list screen

--- src/screens/gifted-chat-list/gifted-list-screen.tsx
+++ src/screens/gifted-chat-list/gifted-list-screen.tsx
@@ -121,12 +121,13 @@ const styles = StyleSheet.create({
   searchContainer: {
     backgroundColor: COLORS.backgroundMainHome,
     marginHorizontal: 16,
-    paddingVertical: 10,
     borderWidth: 1,
     borderColor: COLORS.borderColor,
     borderRadius: 40,
     flexDirection: 'row',
     paddingHorizontal: 12,
+    alignItems: 'center',
+    height: 40,
   },
   searchIcon: {
     width: 15,
